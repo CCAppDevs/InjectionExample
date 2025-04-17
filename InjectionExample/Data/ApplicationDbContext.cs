3435c454d59457980f76addecbc7834d00c9f613
Jesse Harlan <jesse.harlan@centralia.edu>
2025-04-17 10:30:20 -0700

    Add project files.

---
+++ InjectionExample/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using InjectionExample.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace InjectionExample.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+    }
+}
---
+++ InjectionExample/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace InjectionExample.Models
+{
+    public class Address
+    {
+        public int AddressId { get; set; }
+        public string StreetNumber { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Country { get; set; }
+        public string UserId { get; set; }
+        public IdentityUser User { get; set; }
+    }
+}
